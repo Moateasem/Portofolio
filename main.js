f192433a1b9a0c68c594194ea35f3d18bb7c9006
ryan <109682289+Moateasem@users.noreply.github.com>
2022-09-22 23:24:48 +0200

    update main.js

--- main.js
+++ main.js
@@ -20,3 +20,20 @@ $(document).ready(function()
         window.location.hash = hash;
       });
     } // End if
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}

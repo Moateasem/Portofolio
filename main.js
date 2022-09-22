dbdba800109ba953e1c4095b68cd7e98d0e21ccc
ryan <109682289+Moateasem@users.noreply.github.com>
2022-09-22 23:46:11 +0200

    update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+
+
+
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);

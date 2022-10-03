a6e83cc94b70b6aabc6f232fd40766bc5985cc9f
ryan <109682289+Moateasem@users.noreply.github.com>
2022-10-03 06:14:22 +0200

    update main.js

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-// smooth scroll
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        
-        window.location.hash = hash;
-      });
-    } // End 
-  });
-});
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
